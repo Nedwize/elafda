2a8417e0b2f02bbc0372e12b82f0c16bcc1cafa7
Nedwize <nakshatra163@gmail.com>
2024-01-29 01:22:52 +0530

    feat: general pill layout and header design

---
+++ src/app/components/Card.js
@@ -0,0 +1,14 @@
+const Card = () => (
+    <div
+        className="group cursor-pointer rounded-lg border border-transparent p-6 transition-all duration-200 ease-in-out hover:border-neutral-600 hover:bg-opacity-30 border-neutral-700 bg-neutral-800 bg-opacity-20"
+        target="_blank"
+        rel="noopener noreferrer"
+    >
+        <h2 className={`mb-3 text-2xl font-semibold`}>Ravi Sutanjani Fiasco</h2>
+        <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Learn about Next.js in an interactive course with&nbsp;quizzes!
+        </p>
+    </div>
+)
+
+export default Card
---
+++ src/app/components/Pill.js
@@ -0,0 +1,7 @@
+const Pill = ({ title }) => (
+    <div className="transition-all duration-200 ease-in-out hover:border-neutral-600 hover:bg-opacity-30 border-neutral-700 bg-neutral-800 bg-opacity-20 rounded-full text-sm py-1 px-3 font-extralight border cursor-pointer">
+        Indian Tech
+    </div>
+)
+
+export default Pill
